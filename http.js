6de2b737c78bbe3d031527e9b09ccbc03fe403ab
Beau <beausouparphone@gmail.com>
2025-05-01 13:16:22 -0400

    Split the Query String

--- http.js
+++ http.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var url = require('url');
 
 // create a server object
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(req.url); //write a response to the client
-    res.end(); //end the message
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + ' ' + q.month;
+    res.end(txt); //end the message
 }).listen(8080); //the server object listens on port 8080
