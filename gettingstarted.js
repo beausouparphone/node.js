4ec53d8128d09983615556bfabf6f92ebf457798
Beau <beausouparphone@gmail.com>
2025-05-01 12:57:20 -0400

    Include module to gettingstarted

--- gettingstarted.js
+++ gettingstarted.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var dt = require('./module');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hello World!');
+    res.write('The date and time are currently: ' + dt.dateTime());
+    res.end();
 }).listen(8080);
